d57464e4d1f52da3a9009635d63a1129c12ee2f8
sathya <sathyaarhr@gmail.com>
2021-02-12 13:31:41 +0530

    second commit

---
+++ mavenOrangehrm/src/test/java/com/orangehrm/testscript/testConfigureleave.java
@@ -0,0 +1,15 @@
+package com.orangehrm.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.orangehrm.generic.BaseClass;
+
+
+public class testConfigureleave extends BaseClass{
+	
+		@Test
+		public void leavemodule() {
+			Reporter.log("configureleave",true);
+		}
+}
